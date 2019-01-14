9dd4df012255a123bb58e9d4a8aa7a500c7e9da3
Benzaoui <benzaoui.soufiane@dataimpact.fr>
2019-01-14 10:52:22 +0100

    Update middleware repository

--- prxr/middleware.py
+++ prxr/middleware.py
@@ -1,50 +1,37 @@
-import os
-import logging
-import warnings
-from collections import defaultdict
 
-from six.moves.urllib.parse import urlparse
-from w3lib.http import basic_auth_header
+import logging
 from scrapy import signals
-from scrapy.resolver import dnscache
-from scrapy.exceptions import ScrapyDeprecationWarning
-from twisted.internet.error import ConnectionRefusedError, ConnectionDone
+
+
+logger = logging.getLogger(__name__)
 
 
 class PRXRMiddleware(object):
-    url = 'http://127.0.0.1:80'
+    url = None
 
     download_timeout = 190
-    # Handle crawlera server failures
-    connection_refused_delay = 90
+    # connection_refused_delay = 90
     preserve_delay = False
     header_prefix = '-'
-
+    country = 'EN'
+    # r_header = 
     _settings = [
-        ('apikey', str),
-        ('user', str),
-        ('pass', str),
+        # ('r_header', bool),
+        # ('user', str),
+        ('country', str),
         ('url', str),
-        ('maxbans', int),
         ('download_timeout', int),
-        ('preserve_delay', bool),
+        # ('preserve_delay', bool),
     ]
-
 
     def __init__(self, crawler):
         self.crawler = crawler
-        # self.job_id = os.environ.get('SCRAPY_JOB')
-        # self._bans = defaultdict(int)
-        # self._saved_delays = defaultdict(lambda: None)
-
 
     @classmethod
     def from_crawler(cls, crawler):
         o = cls(crawler)
         crawler.signals.connect(o.open_spider, signals.spider_opened)
         return o
-
-
 
     def open_spider(self, spider):
         self.enabled = self.is_enabled(spider)
@@ -53,17 +40,21 @@ class PRXRMiddleware(object):
 
         for k, type_ in self._settings:
             setattr(self, k, self._get_setting_value(spider, k, type_))
-
+        logger.info("Using PRXR")
 
     def is_enabled(self, spider):
         """Hook to enable middleware by custom rules."""
-        
         return (
             getattr(spider, 'prxr_enabled', False) or
             self.crawler.settings.getbool("PRXR_ENABLED")
         )
 
-
+    def _get_setting_value(self, spider, k, type_):
+        o = getattr(self, k, None)
+        s = self._settings_get(
+            type_, 'PRXR_' + k.upper(), o)
+        return getattr(
+            spider, 'prxr_' + k,  s)
 
     def _settings_get(self, type_, *a, **kw):
         if type_ is int:
@@ -77,26 +68,17 @@ class PRXRMiddleware(object):
         else:
             return self.crawler.settings.get(*a, **kw)
 
-
-    def _get_setting_value(self, spider, k, type_):
-        o = getattr(self, k, None)
-        s = self._settings_get(
-            type_, 'PRXR_' + k.upper(), o)
-        return getattr(
-            spider, 'prxr_' + k,  s)
-
     def process_request(self, request, spider):
         if self._is_enabled_for_request(request):
-            self._set_crawlera_default_headers(request)
             request.meta['proxy'] = self.url
-            # request.meta['download_timeout'] = self.download_timeout
-            # request.headers['Proxy-Authorization'] = self._proxyauth
-
+            request.meta['download_timeout'] = self.download_timeout
+            self.crawler.stats.inc_value('prxr/request_count')
+            self.crawler.stats.inc_value('prxr/request/method/%s' % request.method)
+            if self.country:
+                request.headers['country'] = self.country
 
     def process_response(self, request, response, spider):
-        if not self._is_enabled_for_request(request):
-            return response
-
+        return response
 
     def _is_enabled_for_request(self, request):
-        return self.enabled and not request.meta.get('dont_proxy', False)
+        return self.enabled
